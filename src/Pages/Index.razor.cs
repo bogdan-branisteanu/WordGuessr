98d0c922a793e302b67aa3392b413f4ebe43074a
Adi <adi.survlad@yahoo.com>
2022-01-29 23:20:02 +0100

    Add KeyboardEvent

--- src/Pages/Index.razor.cs
+++ src/Pages/Index.razor.cs
@@ -1,10 +1,28 @@
-
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
 
 namespace src.Pages
 {
-    public partial class Index
+    public partial class Index 
     {
         int numLetters = 5;
+        private ElementReference inputDiv;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await inputDiv.FocusAsync();
+            }
+        }
+
+        private void KeyboardEventHandler(KeyboardEventArgs args)
+        {
+            Console.WriteLine("Key Pressed is " + args.Key);
+        }
+
+        
+
 
 
 

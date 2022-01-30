39e7d973866d5938fee83922676f9ebd50d3f3d4
Bogdan Branisteanu <branisteanu.bogdan.ciprian@gmail.com>
2022-01-30 20:02:07 +0000

    Add files for words with length different than five

--- src/Pages/Index.razor.cs
+++ src/Pages/Index.razor.cs
@@ -49,20 +49,23 @@ namespace src.Pages
          string Path;
          switch(this.NumLetters)
          {
+            case 0:
+               Path = String.Concat(Directory.GetCurrentDirectory(), @"\Resources\CommonWordsEN.txt");
+            break;
             case 3:
-               Path = String.Concat(Directory.GetCurrentDirectory(), @"\resources\threeLetterWords.txt");
+               Path = String.Concat(Directory.GetCurrentDirectory(), @"\Resources\3LetterCommonWordsEN.txt");
             break;
             case 4:
-               Path = String.Concat(Directory.GetCurrentDirectory(), @"\resources\fourLetterWords.txt");
+               Path = String.Concat(Directory.GetCurrentDirectory(), @"\Resources\4LetterCommonWordsEN.txt");
             break;
             case 5:
-               Path = String.Concat(Directory.GetCurrentDirectory(), @"\resources\fiveLetterWords.txt");
+               Path = String.Concat(Directory.GetCurrentDirectory(), @"\Resources\5LetterCommonWordsEN.txt");
             break;
             case 6:
-               Path = String.Concat(Directory.GetCurrentDirectory(), @"\resources\sixLetterWords.txt");
+               Path = String.Concat(Directory.GetCurrentDirectory(), @"\Resources\6LetterCommonWordsEN.txt");
             break;
             case 7:
-               Path = String.Concat(Directory.GetCurrentDirectory(), @"\resources\sevenLetterWords.txt");
+               Path = String.Concat(Directory.GetCurrentDirectory(), @"\Resources\7LetterCommonWordsEN.txt");
             break;
             default:
                Path = "invalid";
@@ -73,13 +76,14 @@ namespace src.Pages
             string[] lines = File.ReadAllLines(@Path);
             Random rdn = new Random();
             int index = rdn.Next(0, lines.Length);
+            Console.WriteLine("No of lines: " + lines.Length + " index: " + index);
             this.Word = new WordInstance(lines[index]);
-            this.Word.checkProfanity(String.Concat(Directory.GetCurrentDirectory(), @"\resources\profanityWords.txt"));
+            this.Word.checkProfanity(String.Concat(Directory.GetCurrentDirectory(), @"\resources\ProfanityWordsEN.txt"));
             while(this.Word.ProfanitySafe == false)
             {
                index = rdn.Next(0, lines.Length);
                this.Word = new WordInstance(lines[index]);
-               this.Word.checkProfanity(String.Concat(Directory.GetCurrentDirectory(), @"\resources\profanityWords.txt"));
+               this.Word.checkProfanity(String.Concat(Directory.GetCurrentDirectory(), @"\resources\ProfanityWordsEN.txt"));
             }
          }
          else Console.WriteLine("Specified path for " + @Path + " does not exist!");
@@ -96,15 +100,16 @@ namespace src.Pages
       public void runGame()
       {
          Console.WriteLine(this.NumLetters);
-         if(this.NumLetters == 0)
+         if(this.NumLetters == 0 || this.NumLetters >= 3 && this.NumLetters <= 7)
          {
-            Random rdn = new Random();
-            this.NumLetters = rdn.Next(3,8);
+            Game newGame = new Game(this.NumLetters);
+            newGame.chooseWord();
+            Console.WriteLine(newGame.getWord());
          }
-         Game newGame = new Game(this.NumLetters);
-         newGame.chooseWord();
-         Console.WriteLine(newGame.getWord());
-         
+         else
+         {
+            Console.WriteLine("Invalid number of letters: " + this.NumLetters);
+         }
       }
 
       public void setNumLetters(int numLetters)

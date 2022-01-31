27e127025660f21f9b247afe72b2721678313401
Bogdan Branisteanu <branisteanu.bogdan.ciprian@gmail.com>
2022-01-31 03:53:26 +0000

    Add functional basic virtual keyboard on screen

--- src/Pages/Index.razor.cs
+++ src/Pages/Index.razor.cs
@@ -10,7 +10,7 @@ namespace src.Pages
     {
         int numLetters = 5;
         int i = 1;
-        int j = 10;
+        int j = 1;
         private ElementReference inputDiv;
 
         List<Tile> tileList = new List<Tile>();
@@ -26,9 +26,9 @@ namespace src.Pages
                   Tile tile = new Tile();
                   tile.tileId = j * 10 + i;
 
-                  if (!tileList.Contains(tile)) {
-                     tileList.Add(tile);
-
+                  if (!tileList.Contains(tile))
+                  {
+                    tileList.Add(tile);
                   }
                   
                }
@@ -45,46 +45,142 @@ namespace src.Pages
         }
 
     }
+    
+    private void EnterEventHandler()
+    {
+        Console.WriteLine("The word has been submitted!");
+        // form the word using the key of each tile
+        // check if word is in the corespondent word file
+            // if it is then accept the input, and check with the given word
+            // modify the tiles status based on each key
+            // check if the game is over
+                // if it is, give a game is over popup managing the outcome
+            // else:
+                // j = j + 1
+                // i = 1
+        // else:
+            // throw incorrect word exception
+            // clear all tiles
+            // i = 1
 
-
-
-
+    }
 
     private void KeyboardEventHandler(KeyboardEventArgs args)
     {
-
         Console.WriteLine("Key Pressed is " + args.Key);
-        foreach (Tile tile in tileList)
+        if(args.Key.ToUpper() == "ENTER")
         {
-            if (tile.tileId == j + i)
+            this.EnterEventHandler();
+        }
+        else
+        {
+            Console.WriteLine(i + " " + j);
+            foreach (Tile tile in tileList)
             {
-                tile.Letter = args.Key;
-                tile.State  = "correct";
-                Console.WriteLine(tile.tileId + " " + tile.Letter);
-                
-
-
+                if (tile.tileId == j*10 + i)
+                {
+                    tile.Letter = args.Key.ToUpper();
+                    tile.State  = "correct";
+                    Console.WriteLine(tile.tileId + " contains " + tile.Letter);
+                }
+            }
+            if(!(i == 5 && j == 6))
+            {
+                i++;
+                if (i == 6)
+                {
+                    i = 1;
+                    j += 1;
+                }
             }
         }
-
-        i++;
-
-        if (i == 6)
-        {
-            i = 1;
-            j += 10;
-            if (j == 6)
-            {
-                j = 1;
-            }
-        }
-
         
-      
     }
 
+    private void ButtonEventHandler(string key)
+    {
+        Console.WriteLine("Virtual Key Pressed is " + key);
+        if(key == "backspace")
+        {   
+            // the decementation has to be done after the tile is found
+            // as when it gets to the last tile i=5, j=6, it needs to delete
+            // its content if there is one, otherwise, it need to decrement
+            // the i again to tile i=4, j=6 and delete its content
+            // quite a corner case
 
-
+            // EDIT: PLEASE CHECK BUT I THINK I VE DONE IT
+            
+            Boolean deleted = false;
+            foreach (Tile tile in tileList)
+            {
+                if (tile.tileId == j*10 + i)
+                {
+                    if(tile.Letter != "")
+                    {   
+                        tile.Letter = "";
+                        tile.State  = "default";
+                        Console.WriteLine(tile.tileId + " delete " + tile.Letter);
+                        deleted = true;
+                    }
+                }
+            }
+            if(deleted == false && i != 1 || j != 1 && i != 5 || j != 6) 
+            {
+                i--;
+                if (i == 0)
+                {
+                    i = 5;
+                    j -= 1;
+                    if(j == 0)
+                    {
+                        i = 1;
+                        j = 1;
+                    }
+                }
+                foreach (Tile tile in tileList)
+                {
+                    if (tile.tileId == j*10 + i)
+                    {
+                        if(tile.Letter != "")
+                        {   
+                            tile.Letter = "";
+                            tile.State  = "default";
+                            Console.WriteLine(tile.tileId + " delete " + tile.Letter);
+                            deleted = true;
+                        }
+                    }
+                }
+            }
+            
+        }
+        else if(key == "enter")
+        {
+            this.EnterEventHandler();
+        }
+        else
+        {
+            Console.WriteLine(i + " " + j*10);
+            foreach (Tile tile in tileList)
+            {
+                if (tile.tileId == j*10 + i)
+                {
+                    tile.Letter = key;
+                    tile.State  = "correct";
+                    Console.WriteLine(tile.tileId + " contains " + tile.Letter);
+                }
+            }
+            if(!(i == 5 && j == 6))
+            {
+                i++;
+                if (i == 6)
+                {
+                    i = 1;
+                    j += 1;
+                }
+            }
+        }
+        
+    }
 
 
 
